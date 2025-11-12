efc516770bf83e8e8603fd169bc73a7bd7b66f35
CAndresSierra <camilofullstack@gmail.com>
2025-11-11 23:11:04 -0500

    refactor(core): Implement reactive input stream
    Refactor Main.java to remove unused imports.
    Implement a reactive input stream in ReactiveIntro
    using RxJava to read names from the console.

---
+++ src/main/java/com/devtalles/proyect/observer/ObserverExample01.java
@@ -0,0 +1,40 @@
+package com.devtalles.proyect.observer;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class ObserverExample01 {
+    public static void main(String[] args) {
+        Observable<String> courseStream = Observable.fromIterable(Arrays.asList("HTML", "CSS", null, "C#").stream().filter(Objects::nonNull).toList());
+
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                System.out.println("Estoy susscrito");
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+                System.out.println("Recibi: " + s);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("Fin de la aplication");
+            }
+        };
+
+        courseStream.subscribe(observer);
+
+    }
+}
