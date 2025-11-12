4f9e0852bd18467248015c13ef95a5373a0eed43
CAndresSierra <camilofullstack@gmail.com>
2025-11-11 22:49:57 -0500

    refactor(core): Implement reactive input stream
    Refactor Main.java to remove unused imports.
    Implement a reactive input stream in ReactiveIntro
    using RxJava to read names from the console.

--- src/main/java/com/devtalles/proyect/Main.java
+++ src/main/java/com/devtalles/proyect/Main.java
@@ -1,7 +1,5 @@
 package com.devtalles.proyect;
 
-import io.reactivex.rxjava3.core.Observable;
-
 public class Main {
     public static void main(String[] args){
 
--- src/main/java/com/devtalles/proyect/reactivePrograming/ReactiveIntro.java
+++ src/main/java/com/devtalles/proyect/reactivePrograming/ReactiveIntro.java
@@ -1,17 +1,44 @@
 package com.devtalles.proyect.reactivePrograming;
 
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+
+import java.util.Scanner;
 
 public class ReactiveIntro {
     public static void main(String[] args) {
 
-        //First Example
-        Observable<String> courseStream = Observable.just("HTML", "CSS", "JAVA", "C#");
+//        //First Example
+//        Observable<String> courseStream = Observable.just("HTML", "CSS", "JAVA", "C#");
+//
+//        courseStream.subscribe(
+//                (item) -> System.out.println("Recibido: " + item),
+//                (error) -> System.out.println(error.getMessage()),
+//                () -> System.out.println("Finalizado")
+//        );
 
-        courseStream.subscribe(
-                (item) -> System.out.println("Recibido: " + item),
+        ObservableOnSubscribe subscribe = (emitter) -> {
+            Scanner sc = new Scanner(System.in);
+            String name;
+            while(true){
+                System.out.println("Ingrese un nombre o salir");
+                name = sc.nextLine();
+                if(name.equalsIgnoreCase("salir")){
+                    emitter.onComplete();
+                    break;
+                }
+                emitter.onNext(name);
+            }
+        };
+
+        Observable<String> names = Observable.create(subscribe);
+
+        names.subscribe(
+                item -> System.out.println("Name: " + item),
                 (error) -> System.out.println(error.getMessage()),
-                () -> System.out.println("Finalizado")
+                () -> System.out.println("Finalizando...")
         );
+
+
     }
 }
